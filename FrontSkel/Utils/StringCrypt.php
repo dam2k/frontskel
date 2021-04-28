0a3fc2d0c32ed8c8015e6c8df8fa39416655d6ec
Dino Ciuffetti <dam2000@gmail.com>
2021-04-28 11:56:47 +0200

    it's my code :)

--- FrontSkel/Utils/StringCrypt.php
+++ FrontSkel/Utils/StringCrypt.php
@@ -1,5 +1,12 @@
 <?php
 // vim: expandtab:ts=4:sw=4
+
+/**
+ * FrontSkel: PHP slim based micro framework and WEB frontend
+ * License: MIT
+ * Author: Dino Ciuffetti <dam2000@gmail.com>
+ */
+
 declare(strict_types=1);
 
 namespace FrontSkel\Utils;
