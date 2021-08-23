a1aef3e389275bfea5059c9145e26f9133962d63
Dino Ciuffetti <dam2000@gmail.com>
2021-08-24 00:08:36 +0200

    small makeup

--- FrontSkel/App.php
+++ FrontSkel/App.php
@@ -50,11 +50,11 @@ class App
             throw new ConfigNotValidException("The configuration file \"$config\" cannot be found or read");
         }
         $builder = new \DI\ContainerBuilder();
-	// load the config file to get the tmpDir
-	$allsettings=require($config);
+        //load the config file to get the tmpDir
+        $allsettings=require($config);
         $builder->enableCompilation($allsettings['settings']['tmpDir']);
-	$builder->writeProxiesToFile(true, $allsettings['settings']['tmpDir'].'/proxies');
-	unset($allsettings);
+        $builder->writeProxiesToFile(true, $allsettings['settings']['tmpDir'].'/proxies');
+        unset($allsettings);
         // application configuration file using PHP-DI PHP configuration format (https://php-di.org/doc/php-definitions.html)
         $builder->addDefinitions($config);
         
@@ -85,18 +85,6 @@ class App
             // https://www.doctrine-project.org/projects/doctrine-dbal/en/2.11/index.html
             'AuthDbConnection' => function (ContainerInterface $c): \Doctrine\DBAL\Connection {
                 $settings = $c->get('settings')['auth_db'];
-                /*
-                $connectionParams = array(
-                    user (string): Username to use when connecting to the database.
-                    password (string): Password to use when connecting to the database.
-                    host (string): Hostname of the database to connect to.
-                    port (integer): Port of the database to connect to.
-                    dbname (string): Name of the database/schema to connect to.
-                    unix_socket (string): Name of the socket used to connect to the database.
-                    charset (string): The charset used when connecting to the database.
-                );
-                */
-                
                 // https://www.doctrine-project.org/projects/doctrine-dbal/en/2.11/reference/platforms.html#platforms
                 if(isset($settings['platform'])&&($settings['platform']!="")) { // override doctrine DB autoscouting
                     $myPlatform = new $settings['platform']();
@@ -129,7 +117,6 @@ class App
                 return $smarty;
             },
         ]);
-        
         return $builder->build();
     }
     
@@ -138,9 +125,6 @@ class App
      */
     private function setupApplicationMiddlewares(): void {
         // 1) ContentLengthMiddleware
-        /*$contentLengthMiddleware = new \Slim\Middleware\ContentLengthMiddleware();
-        $this->slimapp->add($contentLengthMiddleware);      // ^ Add Content-Length response header based on Response weight
-        */
         $this->slimapp->add(ContentLength::class);
         
         // 2) Slim addBodyParsingMiddleware() and addRoutingMiddleware()
@@ -156,15 +140,11 @@ class App
         // TODO: check CSP middleware: https://github.com/middlewares/csp
         // TODO: check CORS middleware: https://github.com/middlewares/cors
         // TODO: check Content Negotiation: https://github.com/gofabian/negotiation-middleware
-        /*
-        // CSRF --> https://github.com/slimphp/Slim-Csrf
-        $responseFactory = $this->slimapp->getResponseFactory();
-        */
+        // TODO: check CSRF protection: https://github.com/slimphp/Slim-Csrf
         
         // 4) Error handlers
         $customErrorHandler = new CustomErrorHandler($this->slimapp->getCallableResolver(), $this->slimapp->getResponseFactory(), $this->c);
         $customErrorHandler->registerErrorRenderer('text/html', CustomErrorRenderer::class);
-        
         /*
          * @param bool $displayErrorDetails -> Should be set to false in production
          * @param bool $logErrors -> Parameter is passed to the default ErrorHandler
@@ -176,10 +156,9 @@ class App
     
     /**
      * Setup routing - user should extend this class and override this method with its personal slim routers
+     * http://www.slimframework.com/docs/v4/objects/routing.html
      */
     protected function setupRouting(): void {
-        // http://www.slimframework.com/docs/v4/objects/routing.html
-        // FRONTEND
         // NOTE: middlewares are executed in reverse order! last mw <<< mw <<< mw <<< first mw
         /*
         $this->slimapp->get('/', [FERouter::class, 'GETSlash'])->setName('getslash')->add(CheckValidAuthToken::class);
@@ -188,37 +167,11 @@ class App
         $this->slimapp->get('/logout', [FERouter::class, 'GETLogout'])->setName('getlogout')->add(DropAuthToken::class);
         $this->slimapp->post('/login', [FERouter::class, 'POSTLogin'])->setName('postlogin')->add(ReleaseAuthTokens::class)->add(CheckEmailPwd::class);
         */
-        
-        // AUTH BACKEND
-        /*
-        $this->slimapp->get('/be/auth/v1/checkUserPwd', [BEAuthV1Router::class, ''])->setName('checkUserPwd');
-        */
     }
-    
-    /**
-     * Setup routing (Installer)
-     * https://www.slimframework.com/docs/v4/objects/routing.html
-     */
-    #private function setupRoutingInstaller(): void {
-    #    // http://www.slimframework.com/docs/v4/objects/routing.html
-    #    // NOTE: middlewares are executed in reverse order! last mw <<< mw <<< mw <<< first mw
-    #    $this->slimapp->get('/', [InstallerRouter::class, 'GETSlash'])->setName('getslash');
-    #    $this->slimapp->get('/favicon.ico', [InstallerRouter::class, 'getFavicon'])->setName('getfavicon');
-    #    $this->slimapp->map(['GET', 'POST'], '/install[/{step}]', [InstallerRouter::class, 'GETSlashInstall'])->setName('getslashinstall');
-    #}
     
     // config file must be a valid PHP-DI PHP configuration file: https://php-di.org/doc/php-definitions.html
     public function __construct(String $config) {
-        //$installing=false;
-        //try {
-            $this->c = $this->initializeContainer($config);
-        //} catch(\FrontSkel\Exceptions\ConfigNotValidException $e) { // cannot open config file
-            /*
-            $installing=true;
-            $this->c = $this->initializeContainer('../etc/config_install.php');
-            $this->c->set('configFileNew', $config);
-            */
-        //}
+        $this->c = $this->initializeContainer($config);
         $this->slimapp = Bridge::create($this->c); // use slim-bridge to create the slim app: https://php-di.org/doc/frameworks/slim.html
         if(substr($this->c->get('settings')['basePath'], -1) == '/') { // fuck! I said in the config comment to not put a trailing slash, even if basepath is / !!!
             $msg='Please remove trailing slash from basePath parameter in config file!!';
@@ -227,36 +180,12 @@ class App
         }
         $this->slimapp->setBasePath($this->c->get('settings')['basePath']);
         $this->setupApplicationMiddlewares();
-        
-        // LOG
-        //$this->c->get(LoggerInterface::class)->info("porcozio che notizia, ma come l'ha messa dentro guarda!");
-        
-        // DB
-        /*
-        try {
-            //$conn=$this->c->get(Connection::class);
-            //$platform = $conn->getDatabasePlatform();
-            //print_r($platform);
-            $sql = "SELECT * FROM re WHERE id = :id";
-            $stmt = $conn->prepare($sql);
-            $stmt->bindValue('id', 1); // map id to 1
-            $stmt->execute();
-            print_r($stmt->fetchAssociative());
-        } catch (\Exception $e) {
-            echo "Beccato... " . $e->getMessage();
-        }
-        */
-        
+        // LOG something
+        //$this->c->get(LoggerInterface::class)->info("Porcozio che notizia, ma come l'ha messa dentro guarda!");
         $routeCollector = $this->slimapp->getRouteCollector();
         $routeCollector->setDefaultInvocationStrategy(new RequestResponse());
         $routeCollector->setCacheFile($this->c->get('settings')['tmpDir'].'/routercache.tmp');
-        
-        //if(!$installing) {
         $this->setupRouting();
-        /*} else  {
-            $this->setupRoutingInstaller();
-        }*/
-        
         // run the application
         $this->slimapp->run();
     }
